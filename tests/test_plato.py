bbfc6429af48517e53a9b0ce8830e7f91e74a564
Yoav Ram <yoavram+github@gmail.com>
2015-04-28 08:12:10 +0300

    download and upload tests uncommented and work on local chromedriver

--- tests/test_plato.py
+++ tests/test_plato.py
@@ -63,46 +63,46 @@ class PlatoTestCase(unittest.TestCase):
         self.assertTrue("github.com" in driver.page_source)
 
 
-    # def test_upload(self):
-    #     driver = self.driver        
-    #     driver.get(self.site_url)
-    #     self.assertTrue("Plato" in driver.title)
-    #     container = driver.find_element_by_class_name('container')
-    #     assert container != None
-    #     upload_file = driver.find_element_by_id("upload-file")
-    #     assert upload_file != None
-    #     upload_file.send_keys(os.getcwd() + r"\tests\empty.csv")
-    #     strains = driver.find_elements_by_class_name('input-strain')
-    #     assert len(strains) == 1, len(strains)
-    #     strain = strains[0]
-    #     assert strain.get_attribute('value') == '0', strain.get_attribute('value')
-    #     assert strain.get_attribute('style') == 'background: rgb(255, 255, 255);', strain.get_attribute('style')
+    def test_upload(self):
+        driver = self.driver        
+        driver.get(self.site_url)
+        self.assertTrue("Plato" in driver.title)
+        container = driver.find_element_by_class_name('container')
+        assert container != None
+        upload_file = driver.find_element_by_id("upload-file")
+        assert upload_file != None
+        upload_file.send_keys(os.getcwd() + r"\tests\empty.csv")
+        strains = driver.find_elements_by_class_name('input-strain')
+        assert len(strains) == 1, len(strains)
+        strain = strains[0]
+        assert strain.get_attribute('value') == '0', strain.get_attribute('value')
+        assert strain.get_attribute('style') == 'background: rgb(255, 255, 255);', strain.get_attribute('style')
         
 
-    # def test_download(self):
-    #     driver = self.driver
-    #     driver.get(self.site_url)
-    #     btn = driver.find_element_by_class_name('octicon-arrow-down')
-    #     assert btn != None
-    #     btn.click()
-    #     btn_download = self.driver.find_element_by_id('btnDownload')
-    #     assert btn_download != None
-    #     href = btn_download.get_attribute('href')
+    def test_download(self):
+        driver = self.driver
+        driver.get(self.site_url)
+        btn = driver.find_element_by_class_name('octicon-arrow-down')
+        assert btn != None
+        btn.click()
+        btn_download = self.driver.find_element_by_id('btnDownload')
+        assert btn_download != None
+        href = btn_download.get_attribute('href')
         
-    #     script = """var x = new XMLHttpRequest();
-    #     x.onload = function() {
-    #         var div = document.createElement('div');
-    #         div.id = 'test';
-    #         div.innerText = x.responseText;
-    #         document.body.appendChild(div)            
-    #     };
-    #     x.open('get', '%s');
-    #     x.send();""" % href        
-    #     driver.execute_script(script)
-    #     test_div = driver.find_element_by_id('test')
-    #     with open("tests/plate.csv") as f:
-    #         plate_text = f.read()
-    #     assert test_div.text == plate_text, tet_div.text
+        script = """var x = new XMLHttpRequest();
+        x.onload = function() {
+            var div = document.createElement('div');
+            div.id = 'test';
+            div.innerText = x.responseText;
+            document.body.appendChild(div)            
+        };
+        x.open('get', '%s');
+        x.send();""" % href        
+        driver.execute_script(script)
+        test_div = driver.find_element_by_id('test')
+        with open("tests/plate.csv") as f:
+            plate_text = f.read()
+        assert test_div.text == plate_text, tet_div.text
 
 
     def tearDown(self):
