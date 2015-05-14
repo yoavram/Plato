703e53fe73de89a3ec3c5e0f1398fe91b0ff4460
Yoav Ram <yoavram+github@gmail.com>
2015-05-14 13:38:55 +0300

    test download and upload back

--- tests/test_plato.py
+++ tests/test_plato.py
@@ -83,48 +83,48 @@ class PlatoTestCase(unittest.TestCase):
         self.assertTrue("github.com" in driver.page_source)
 
 
-    # def test_upload(self):
-    #     driver = self.driver        
-    #     driver.get(self.site_url)
-    #     self.assertTrue("Plato" in driver.title)
-    #     container = driver.find_element_by_class_name('container')
-    #     assert container != None
-    #     upload_file = driver.find_element_by_id("upload-file")
-    #     assert upload_file != None
-    #     driver.execute_script("arguments[0].style.display='inline';", upload_file) # make input visible so IE can interact with it
-    #     upload_file.send_keys(os.path.join(os.getcwd(), "tests", "empty.csv"))
-    #     strains = driver.find_elements_by_class_name('input-strain')
-    #     assert len(strains) == 1, len(strains)
-    #     strain = strains[0]
-    #     assert strain.get_attribute('value') == '0', strain.get_attribute('value')
-    #     assert strain.get_attribute('style') == 'background: rgb(255, 255, 255);', strain.get_attribute('style')
+    def test_upload(self):
+        driver = self.driver        
+        driver.get(self.site_url)
+        self.assertTrue("Plato" in driver.title)
+        container = driver.find_element_by_class_name('container')
+        assert container != None
+        upload_file = driver.find_element_by_id("upload-file")
+        assert upload_file != None
+        driver.execute_script("arguments[0].style.display='inline';", upload_file) # make input visible so IE can interact with it
+        upload_file.send_keys(os.path.join(os.getcwd(), "tests", "empty.csv"))
+        strains = driver.find_elements_by_class_name('input-strain')
+        assert len(strains) == 1, len(strains)
+        strain = strains[0]
+        assert strain.get_attribute('value') == '0', strain.get_attribute('value')
+        assert strain.get_attribute('style') == 'background: rgb(255, 255, 255);', strain.get_attribute('style')
         
 
-    # def test_download(self):
-    #     driver = self.driver
-    #     driver.get(self.site_url)
-    #     btn = driver.find_element_by_class_name('octicon-arrow-down')
-    #     assert btn != None
-    #     btn.click()
-    #     btn_download = self.driver.find_element_by_id('btnDownload')
-    #     assert btn_download != None
-    #     href = btn_download.get_attribute('href')
+    def test_download(self):
+        driver = self.driver
+        driver.get(self.site_url)
+        btn = driver.find_element_by_class_name('octicon-arrow-down')
+        assert btn != None
+        btn.click()
+        btn_download = self.driver.find_element_by_id('btnDownload')
+        assert btn_download != None
+        href = btn_download.get_attribute('href')
         
-    #     script = """var x = new XMLHttpRequest();
-    #     x.onload = function() {
-    #         var div = document.createElement('div');
-    #         div.id = 'test';
-    #         div.innerText = x.responseText;
-    #         document.body.appendChild(div)            
-    #     };
-    #     x.open('get', '%s');
-    #     x.send();""" % href        
-    #     driver.execute_script(script)
-    #     test_div = driver.find_element_by_id('test')
-    #     with open(os.path.join(os.getcwd(), "tests", "plate.csv")) as f:
-    #         plate_text = f.read()
-    #     assert len(test_div.text) == len(plate_text), len(test_div.text)
-    #     assert test_div.text == plate_text, test_div.text
+        script = """var x = new XMLHttpRequest();
+        x.onload = function() {
+            var div = document.createElement('div');
+            div.id = 'test';
+            div.innerText = x.responseText;
+            document.body.appendChild(div)            
+        };
+        x.open('get', '%s');
+        x.send();""" % href        
+        driver.execute_script(script)
+        test_div = driver.find_element_by_id('test')
+        with open(os.path.join(os.getcwd(), "tests", "plate.csv")) as f:
+            plate_text = f.read()
+        assert len(test_div.text) == len(plate_text), len(test_div.text)
+        assert test_div.text == plate_text, test_div.text
 
 
     def tearDown(self):
